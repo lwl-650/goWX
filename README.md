b66936d1c02e6728b3f5b2e025036d259cae8088
无敌暴龙战士 <75242799+lwl-650@users.noreply.github.com>
2022-08-03 11:48:28 +0800

    fix: 完成回复消息

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 1. https://github.com/silenceper/wechat/blob/master/README.md
 
 2. https://www.cnblogs.com/taoshihan/p/16506672.html
+
+3. https://www.cnblogs.com/MyUniverse/p/13581884.html
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"reflect"
 
 	wechat "github.com/silenceper/wechat/v2"
 	"github.com/silenceper/wechat/v2/cache"
@@ -12,13 +13,13 @@ import (
 
 func serveWechat(rw http.ResponseWriter, req *http.Request) {
 	wc := wechat.NewWechat()
-	//这里本地内存保存access_token，也可选择redis，memcache或者自定cache
+	//这里本地内存保存access_token，也可选择redis memcache 或者自定cache
 	memory := cache.NewMemory()
 	cfg := &offConfig.Config{
-		AppID:     "xxx",
-		AppSecret: "xxx",
-		Token:     "xxx",
-		// EncodingAESKey: "xxxx",
+		AppID:     "wxf3b2ce464e28d596",
+		AppSecret: "9e04055c8e08d80828fb286ea972488c",
+		Token:     "lwl",
+		// EncodingAESKey: "",
 		Cache: memory,
 	}
 	officialAccount := wc.GetOfficialAccount(cfg)
@@ -26,11 +27,29 @@ func serveWechat(rw http.ResponseWriter, req *http.Request) {
 	// 传入request和responseWriter
 	server := officialAccount.GetServer(req, rw)
 	//设置接收消息的处理方法
-	server.SetMessageHandler(func(msg message.MixMessage) *message.Reply {
-		text := message.NewText(msg.Content)
+	server.SetMessageHandler(func(mixMessage *message.MixMessage) *message.Reply {
+		//TODO
+		//回复消息：演示回复用户发送的消息
+		switch mixMessage.MsgType {
+		//自动回复消息
+		case message.MsgTypeText:
+			text := message.NewText(mixMessage.Content)
+			return &message.Reply{MsgType: message.MsgTypeText, MsgData: text}
+		}
+		switch mixMessage.Event {
+		//如果是初次订阅回复图文消息
+		case message.EventSubscribe:
+			//获取token
+			miniprogrampagemessage := message.NewCustomerMiniprogrampageMessage(reflect.ValueOf(server.RequestMsg.FromUserName).String(), "title", "appId", "pages/index/index", "缩略图id")
+			officialAccount.GetCustomerMessageManager().Send(miniprogrampagemessage)
+			return &message.Reply{MsgType: message.MsgTypeMiniprogrampage, MsgData: miniprogrampagemessage}
+			//根据token 发送客服消息
+
+		}
+		text := message.NewText(mixMessage.Content)
+
 		return &message.Reply{MsgType: message.MsgTypeText, MsgData: text}
 	})
-
 	//处理消息接收以及回复
 	err := server.Serve()
 	if err != nil {
